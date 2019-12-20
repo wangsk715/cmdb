0f769e148f2461fdd132f5c9f42e3f7386bccee8
wangsk715 <wangsk715@163.com>
2019-12-20 12:01:55 +0800

    抽取mysql公共类

--- users/dbuntils.py
+++ users/dbuntils.py
@@ -12,13 +12,20 @@ MYSQL_PASSWD = 'redhat'
 MYSQL_DB = 'cmdb'
 CHARSET = 'utf8'
 
-def execute_mysql():
+def execute_mysql(sql, args, fetch=True, one=False):
     conn = MySQLdb.connect(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASSWD, db=MYSQL_DB,
                            charset=CHARSET)
     cur = conn.cursor(cursors.DictCursor)
     print(cur)
-    cur.execute()
-    resutl = cur.fetchall()
+    cur.execute(sql, args)
+    if fetch:
+        if one:
+            resutl = cur.fetchall()
+        else:
+            result = cur.fetchone()
+    else:
+        conn.commit()
     cur.close()
     conn.close()
-    print(resutl)
+
+
